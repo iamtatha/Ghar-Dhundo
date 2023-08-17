a4e4f99f17dfdb3691b5a673701cecfd4fb246b4
snowy_lone_wolf <57251093+iamtatha@users.noreply.github.com>
2023-08-17 23:46:02 +0530

    Add files via upload

---
+++ assetsdjango/images/javascript/button.js
@@ -0,0 +1,10 @@
+function show_signup() {
+    var x = document.getElementById("hide-name");
+    x.style.display = "block";
+    
+    var x = document.getElementById("hide-dob");
+    x.style.display = "block";
+
+    var x = document.getElementById("hide-cp");
+    x.style.display = "block";
+}
